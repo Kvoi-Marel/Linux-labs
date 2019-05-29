0786531761284300cd45812138e279c94dc50425
Kindface <36567857+Kindface@users.noreply.github.com>
2019-05-29 21:58:03 +0300

    Added beautiful output

--- suzen/suzen19.sh
+++ suzen/suzen19.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-mkdir -p Documents/projects/lab19/using/simple/bash/commands
-echo "Flag suzen 19 is: $(ls -a Documents/projects/lab19/using/simple/bash/commands | egrep '^[0-9a-zA-Z]')"
+echo "Flag suzen 19 is: $(mkdir -p Documents/projects/lab19/using/simple/bash/commands && ls -a Documents/projects/lab19/using/simple/bash/commands | egrep '^[0-9a-zA-Z]')"
