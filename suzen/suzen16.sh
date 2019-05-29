fd324078dea139428c1b7a67cd35c8046486f8c5
Kindface <36567857+Kindface@users.noreply.github.com>
2019-05-29 21:56:45 +0300

    Added beautiful output

--- suzen/suzen16.sh
+++ suzen/suzen16.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#echo "Flag suzen16 is : $(id | awk -F, '{print $12}' | awk -F"(" '{print $2}' | awk -F")" '{
+echo "Flag suzen16 is : $(id | awk -F, '{print $12}' | awk -F"(" '{print $2}' | awk -F")" '{
 print $1}')"
